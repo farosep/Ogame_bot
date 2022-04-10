43b3ee8fa6be91e936f51696fed7ec9fdb89bcbc
farosep <45363655+farosep@users.noreply.github.com>
2022-04-10 18:53:26 +0300

    fix dict and int

--- Decider.py
+++ Decider.py
@@ -70,9 +70,9 @@ def decide_what_tech_to_research(driver, data):
         research_technology(driver, data, 'espionageTechnology')
     elif data.Available_technologies['computerTechnology']:
         research_technology(driver, data, 'computerTechnology')
-    elif data.Available_technologies['energyTechnology'] and data.Technologies_levels < 8:
+    elif data.Available_technologies['energyTechnology'] and data.Technologies_levels['energyTechnology'] < 8:
         research_technology(driver, data, 'energyTechnology')
-    elif data.Available_technologies['laserTechnology'] and data.Technologies_levels < 12:
+    elif data.Available_technologies['laserTechnology'] and data.Technologies_levels['energyTechnology'] < 12:
         research_technology(driver, data, 'laserTechnology')
     elif data.Available_technologies['combustionDriveTechnology']:
         research_technology(driver, data, 'combustionDriveTechnology')
